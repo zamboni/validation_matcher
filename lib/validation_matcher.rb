f70ee83c9edaff7da1e3472fdaffa30f1ff0fe9f
BM5k <me@bm5k.com>
2013-07-23 12:14:26 -0700

    fixup: improve handling of validator options

--- lib/validation_matcher.rb
+++ lib/validation_matcher.rb
@@ -6,27 +6,25 @@ module ValidationMatcher
   RSpec::Matchers.define :validate do |kind|
 
     chain(:of)   { |field| @field            = field }
-    chain(:with) { |hash|  @expected_options = hash  }
-
-    def options_message
-      return '' unless diff.present?
-      str  = "\n expected options: " + @expected_options.inspect
-      str << "\n actual options: "   + validator_options.inspect
-      str
-    end
+    chain(:with) { |hash|  @expected_options = hash }
 
     description do
       msg  = "validate the #{ kind } of #{ @field.inspect }"
       msg << " with options: #{ @expected_options.inspect }" if @expected_options
+      @expected_options = nil # for some reason this appears to be cached between runs?
       msg
     end
 
     failure_message_for_should do
-      "Expected #{ described_class } to validate the #{ kind } of #{ @field.inspect } #{ options_message }"
+      "Expected #{ described_class } to validate the #{ kind } of #{ @field.inspect } #{ diff }"
     end
 
     failure_message_for_should_not do
-      "Expected #{ described_class } not to validate the #{ kind } of #{ @field.inspect } #{ options_message }"
+      "Expected #{ described_class } not to validate the #{ kind } of #{ @field.inspect } #{ diff }"
+    end
+
+    def expected_options
+      @expected_options || {}
     end
 
     def validator
@@ -37,14 +35,25 @@ module ValidationMatcher
       @validator_options ||= validator.options rescue {}
     end
 
+    def diff?
+      # if validator_options is empty and @expected_options is not, there is a problem
+      # if @expected_options is empty and validator_options is not, there is a problem
+      # if neither is empty diff them, if there's a diff, there is a problem
+
+      return true if expected_options.present? ^ validator_options.present?
+      return true if validator_options.diff(expected_options).present?
+      false
+    end
+
     def diff
-      @diff ||= @expected_options ? validator_options.diff(@expected_options) : {}
+      return '' unless diff?
+      str  = "\n expected options: #{ expected_options.inspect }"
+      str << "\n actual options: #{ validator_options.inspect }"
+      str
     end
 
     match do |actual|
-      result = validator.present? and diff.blank?
-      @expected_options = nil # for some reason this appears to be cached between runs?
-      result
+      validator.present? and not diff?
     end
 
   end
--- spec/thing_spec.rb
+++ spec/thing_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 
 describe Thing do
 
-  it { should_not validate(:presence).of(:field_a) }
-  it { should     validate(:presence).of(:field_b) }
-  it { should     validate(:numericality).of(:field_c).with(only_integer: true, allow_nil: false) }
+  it { should_not validate(:presence).of :field_a }
+  it { should     validate(:presence).of :field_b }
+
+  it { should_not validate(:numericality).of :field_c }
+  it { should_not validate(:numericality).of(:field_c).with only_integer: true }
+  it { should     validate(:numericality).of(:field_c).with only_integer: true, allow_nil: false }
 
 end
